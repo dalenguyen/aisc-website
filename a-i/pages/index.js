30f72b8b137d4a5d7b6a7382cd4d65e01004d085
Xiyang Chen <settinghead@gmail.com>
2019-02-07 00:03:03 -0500

    subscribe

--- a-i/pages/index.js
+++ a-i/pages/index.js
@@ -34,7 +34,34 @@ export default () => (
           Coming soon.
         </p>
       </Jumbotron>
-
+      <section className="container text-center">
+        <div className="row">
+          <div className="col-sm-6 offset-sm-3">
+            <link href="//cdn-images.mailchimp.com/embedcode/classic-10_7.css" rel="stylesheet" type="text/css" />
+            <h5 className="card-title">Subscribe to receive updates about us</h5>
+            <div id="mc_embed_signup">
+              <form action="https://science.us20.list-manage.com/subscribe/post?u=c7831af29c0c46bd5ec4c04c7&amp;id=9e6c32b697"
+                method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" className="validate"
+                target="_blank" noValidate>
+                <div id="mc_embed_signup_scroll">
+                  <div className="mc-field-group">
+                    <label htmlFor="mce-EMAIL">Email Address <span className="asterisk">*</span>
+                    </label>
+                    <input type="email" name="EMAIL" className="required email" id="mce-EMAIL" />
+                  </div>
+                  <div
+                    style={{ position: 'absolute', left: '-5000px' }}
+                    aria-hidden="true"><input type="text"
+                      name="b_c7831af29c0c46bd5ec4c04c7_9e6c32b697"
+                      tabIndex="-1" /></div>
+                  <div className="clear text-center"><input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe"
+                    className="button" /></div>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </section>
     </main>
   </Fragment>
 );
