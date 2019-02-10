27b91f6cf288a70efcddf8d870ef4282d81c3df2
Xiyang Chen <settinghead@gmail.com>
2019-02-09 23:37:33 -0500

    build and dev sync contentful

--- scripts/fetch-contentful.js
+++ scripts/fetch-contentful.js
@@ -40,9 +40,3 @@ async function fetchAndSaveType(client, type) {
 
   console.log(`Fetched and saved type "${type}".`);
 }
-
-function fromEntries(iterable) {
-  return [...iterable]
-    .reduce((obj, { 0: key, 1: val }) => Object.assign(obj, { [key]: val }), {})
-}
-
