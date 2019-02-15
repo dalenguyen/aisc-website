5422344fd18c31574510a8ea7ffdc0922ec600e6
Xiyang Chen <settinghead@gmail.com>
2019-02-14 20:19:59 -0500

    fancier fonts

--- tdls/components/themes-and-such.js
+++ tdls/components/themes-and-such.js
@@ -11,7 +11,7 @@ export default () => (
     <script async src="https://www.googletagmanager.com/gtag/js?id=UA-131780670-1"></script>
     <link rel="stylesheet" type="text/css" charSet="UTF-8" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css" />
     <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick-theme.min.css" />
-    <link href="/static/modal-video.css" rel="stylesheet" />
+    <link href="https://fonts.googleapis.com/css?family=Roboto:500,700,500i,700i,900,900i" rel="stylesheet" />
     <link href="/static/main.css" rel="stylesheet" />
     <link rel="icon" type="image/png" href="/static/images/tdls_logo.png" />
   </Fragment>
