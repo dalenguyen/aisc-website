601c87303fddb2a567072308e3a48a88e9287651
Xiyang Chen <settinghead@gmail.com>
2019-02-17 23:09:31 -0500

    mock admin

--- next.config.js
+++ next.config.js
@@ -3,32 +3,36 @@ const webpack = require('webpack');
 const fs = require('fs');
 const path = require('path');
 const withTypescript = require('@zeit/next-typescript');
-const withSass = require('@zeit/next-sass')
+const withSass = require('@zeit/next-sass');
+const withFonts = require('next-fonts');
 
-module.exports = withTypescript(withSass({
+module.exports = withTypescript(
+  withSass(
+    withFonts({
+      webpack: (config) => {
+        config.module.rules.push(
+          {
+            test: /\.md$/,
+            use: 'raw-loader'
+          }
+        );
+        config.plugins.push(
+          new webpack.EnvironmentPlugin(process.env),
+        );
 
-  webpack: (config) => {
-    config.module.rules.push(
-      {
-        test: /\.md$/,
-        use: 'raw-loader'
+        return config
+      },
+
+      exportPathMap: async function (defaultPathMap, config) {
+        const { dir } = config;
+        const { exportPathMap } = require(path.join(dir, 'next.config'));
+
+        if (exportPathMap) {
+          return exportPathMap(defaultPathMap, config);
+        } else {
+          return defaultPathMap;
+        }
       }
-    );
-    config.plugins.push(
-      new webpack.EnvironmentPlugin(process.env),
-    );
-
-    return config
-  },
-
-  exportPathMap: async function (defaultPathMap, config) {
-    const { dir } = config;
-    const { exportPathMap } = require(path.join(dir, 'next.config'));
-
-    if (exportPathMap) {
-      return exportPathMap(defaultPathMap, config);
-    } else {
-      return defaultPathMap;
-    }
-  }
-}))
+    })
+  )
+)
