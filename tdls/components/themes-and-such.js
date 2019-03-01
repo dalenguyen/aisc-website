a297af6ed18f460a48f84da3d179a6f3290b4b52
Xiyang Chen <settinghead@gmail.com>
2019-02-28 22:06:39 -0500

    youtube verify take 2

--- tdls/components/themes-and-such.js
+++ tdls/components/themes-and-such.js
@@ -8,7 +8,7 @@ export default () => (
     <meta name="viewport" content="initial-scale=1.0, width=device-width" />
     <meta name="viewport" content="width=device-width,minimum-scale=1,initial-scale=1" />
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-131780670-1"></script>
+    <meta name="google-site-verification" content="vNQ5Pwbxdj5xT2e0HVRqnjAV88NNjmwNf5NI6k8zdNs" />    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-131780670-1"></script>
     <link href="https://fonts.googleapis.com/css?family=Titillium+Web:400,400i,500,700,500i,700i,900,900i" rel="stylesheet" />
     <link rel="icon" type="image/png" href="/static/images/tdls_logo.png" />
     <link href="/static/main.css" rel="stylesheet" />
