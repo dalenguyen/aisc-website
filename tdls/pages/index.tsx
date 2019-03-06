149a4f431f606453a8ff8069991e530d9511aa57
Xiyang Chen <settinghead@gmail.com>
2019-03-05 21:05:42 -0500

    small tweaks

--- tdls/pages/index.tsx
+++ tdls/pages/index.tsx
@@ -109,6 +109,7 @@ const Index = ({ allEvents }: { allEvents: AllEvents }) => {
             <li data-target="#carouselExampleIndicators" data-slide-to="0" className="active"></li>
             <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
             <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+            <li data-target="#carouselExampleIndicators" data-slide-to="3"></li>
           </ol>
 
           <div className="carousel-inner">
