a42cb49564610c959cea06a07495147ea32b8b9c
Xiyang Chen <settinghead@gmail.com>
2019-03-08 19:28:02 -0500

    no smooth scroll for you!

--- tdls/pages/index.tsx
+++ tdls/pages/index.tsx
@@ -99,7 +99,6 @@ const Index = ({ allEvents }: { allEvents: AllEvents }) => {
         <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css" />
         <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick-theme.min.css" />
         <ThemesAndSuch />
-        <link href="/static/smooth-scroll.css" rel="stylesheet" />
       </Head>
       <EventRoutingHandler />
       <Header allEvents={allEvents} />
