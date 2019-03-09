ab370fa4a72b904815b61aa6e914c8906ed6d910
Xiyang Chen <settinghead@gmail.com>
2019-03-08 19:20:51 -0500

    load full story async

--- tdls/components/shared-body-scripts.js
+++ tdls/components/shared-body-scripts.js
@@ -1,17 +1,22 @@
-import { Fragment } from 'react';
+import { Fragment, useEffect } from 'react';
 import $ from 'jquery-slim';
 
 
-export default () => (
-  <Fragment>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossOrigin="anonymous"></script>
-    <script src="https://apis.google.com/js/platform.js"></script>
-  </Fragment>
-);
+export default () => {
+  useEffect(() => {
+    loadFulLStory();
+  }, []);
+  return (
+    <Fragment>
+      <script async src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
+      <script async src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossOrigin="anonymous"></script>
+      <script src="https://apis.google.com/js/platform.js"></script>
+    </Fragment>
+  );
+}
 
 
-if (typeof window !== 'undefined') {
+function loadFulLStory() {
   // fullstory
 
   window['_fs_debug'] = false;
@@ -36,6 +41,6 @@ if (typeof window !== 'undefined') {
   gtag('js', new Date());
 
   gtag('config', 'UA-131780670-1');
-
 }
+
 
