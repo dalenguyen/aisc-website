b39427babf5822cb1a652565781eed89ad272791
Xiyang Chen <settinghead@gmail.com>
2019-03-22 01:01:25 -0400

    hero shadow

--- aisc/components/hero.tsx
+++ aisc/components/hero.tsx
@@ -6,7 +6,7 @@ function Hero({ title, subtitle }: { title: string | JSX.Element, subtitle?: str
     <div className={`relative tc title-hero`}>
       <div className="backdrop"></div>
       <div className="mw7 center white pv4">
-        <div className="pv4 p-3">
+        <div className="pv4 p-3 text-area">
           <h1 className="f1 normal lh-title ma0 pa0">
             {title}
           </h1>
